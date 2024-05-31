f6dd6fc30fbacd07d3fa994ed8f900c0d46285ea
KACZ3Q <115534849+KACZ3Q@users.noreply.github.com>
2024-05-31 19:50:40 +0200

    first commit

---
+++ src/api/szlak/routes/szlak.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * szlak router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::szlak.szlak');
---
+++ src/api/szlak/services/szlak.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * szlak service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::szlak.szlak');
